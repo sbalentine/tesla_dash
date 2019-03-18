d037f447d53547804c9b2ee9aee7c9eb0c787587
Scott Balentine <scott.balentine@microsoft.com>
2019-03-18 13:59:05 -0700

    connect to tesla api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    tesla_api = TeslaApi::Client.new(ENV['TESLA_ACCOUNT_EMAIL'],
+                                     ENV['TESLA_CLIENT_ID'],
+                                     ENV['TESLA_CLIENT_SECRET'])
+    tesla_api.token = ENV['TESLA_ACCOUNT_TOKEN']
+    model_3 = tesla_api.vehicles.first
+    model_3.wake_up
+    @charge_state = model_3.charge_state
   end
 end
